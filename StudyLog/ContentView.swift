9dd42f9944ce4ea633188bcd58770018fd8b310b
mash-111 <honda.masaaki11@gmail.com>
2026-03-15 19:53:30 +0900

    Initial Commit

---
+++ StudyLog/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StudyLog
+//
+//  Created by Masaaki Honda on 2026/03/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StudyLog/StudyLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyLogApp.swift
+//  StudyLog
+//
+//  Created by Masaaki Honda on 2026/03/15.
+//
+
+import SwiftUI
+
+@main
+struct StudyLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
